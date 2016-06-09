75af598577a5872d41ffe6d8e1a14a4247a6f8cf
Sean Oldham <seanosaur@me.com>
2016-06-08 20:44:49 -0500

    generate Definitions controller, populate controller with logic, simplify routes

---
+++ app/controllers/definitions_controller.rb
@@ -0,0 +1,55 @@
+class DefinitionsController < ApplicationController
+  # before_action :set_term
+  before_action :set_definition, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @definitions = @term.definitions
+  end
+
+  def show
+  end
+
+  def new
+    @definition = @term.definition.new
+  end
+
+  def create
+    @definition = @term.definition.new(definition_params)
+    if @definition.save
+      redirect_to(@term, notice: 'Definition created!')
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @definition.update_attributes(definition_params)
+      redirect_to([@definition.term, @definition], notice: 'Definition updated!')
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @definition.destroy
+
+    redirect_to root_url
+  end
+
+  private
+
+  def set_term
+    @term = Term.find(params[:term_id])
+  end
+
+  def set_definition
+    @definition = @term.definitions.find(params[:id])
+  end
+
+  def definition_params
+    params.require(:definition).permit(:meaning)
+  end
+end
