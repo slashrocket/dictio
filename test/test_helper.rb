b105f3b112075205eec7c1637e1ccd87d1c33c38
Sean Oldham <seanosaur@me.com>
2016-06-21 21:19:45 -0500

    completely remove coveralls leftovers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,5 @@
 require 'simplecov'
 SimpleCov.start
-# require 'coveralls'
-# Coveralls.wear!('rails')
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
