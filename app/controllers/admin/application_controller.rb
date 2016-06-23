903b807f367ea3fa0181accf343e70064f3e7a80
Sean Oldham <seanosaur@me.com>
2016-06-22 20:28:35 -0500

    only allow admins to access /admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,10 +6,11 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    include Clearance::Controller
     before_filter :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to root_path unless signed_in? && current_user.admin?
     end
 
     # Override this value to specify the number of elements to display at a time
