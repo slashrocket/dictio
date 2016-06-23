e6b4e35cd45b512e6d05ad4102670891c657334c
Russell Christensen <russellchristensen@me.com>
2016-06-23 02:10:37 +0000

    Remove password from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623004330) do
+ActiveRecord::Schema.define(version: 20160623020957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20160623004330) do
     t.string   "email"
     t.string   "first_name"
     t.string   "last_initial"
-    t.string   "password"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.string   "encrypted_password", limit: 128
