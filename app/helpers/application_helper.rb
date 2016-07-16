9daa4e18df3b15d97ab7b3c98b6c79da75adede6
Russell Christensen <russellchristensen@me.com>
2016-07-16 19:38:54 +0000

    Begin adding meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 # Application Helper
 module ApplicationHelper
     
+    def meta_tag(tag, text)
+        content_for :"meta_#{tag}", text
+    end
+    
+    def yield_meta_tag(tag, default_text='')
+        content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+    end
+        
+    
     def full_title(page_title = '')
         base_title = "Dictio"
         if page_title.empty?
