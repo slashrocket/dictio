691118714f5b484e00d4498c46063405bd6857c3
Russell Christensen <russellchristensen@me.com>
2016-07-22 11:59:27 +0000

    Added route for profile

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   resources :users,
     controller: 'users',
     only: 'create'
+    
+    get 'profile', to: 'users#show', as: :profile
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
