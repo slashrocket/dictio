963c527024049d806d31104c2edfa976d3dc6cda
Russell Christensen <russellchristensen@me.com>
2016-07-28 02:21:20 +0000

    Added smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   
+  config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 587,
