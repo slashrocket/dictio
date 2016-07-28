08fc8e7a3631f253b97da24ba6741275045eb7a6
Sean Oldham <seanosaur@me.com>
2016-07-27 20:13:40 -0500

    nitpicky things that don’t matter anyway

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,36 @@
 class UsersController < ApplicationController
-    before_action :require_login, only: :show
-    
-    def show
+  before_action :require_login, only: :show
+
+  def show
     @totalTerms = User.totalTerms(current_user)
+  end
+
+  def create
+    @user = user_from_params
+    if @user.save
+      sign_in @user
+      redirect_to root_path
+    else
+      render template: 'users/new'
     end
-    
-    def create
-        @user = user_from_params
-        if @user.save
-            sign_in @user
-            redirect_to root_path
-        else
-            render template: 'users/new'
-        end
-    end
-    
-    private
-    
-    def user_from_params
-        user_params = params[:user] || Hash.new
-        username = user_params.delete(:username)
-        email = user_params.delete(:email)
-        password = user_params.delete(:password)
-    
+  end
+
+  private
+
+  def user_from_params
+    user_params = params[:user] || Hash.new
+    username = user_params.delete(:username)
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+
     Clearance.configuration.user_model.new(user_params).tap do |user|
-        user.username = username
-        user.email = email
-        user.password = password
+      user.username = username
+      user.email = email
+      user.password = password
     end
-    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
+  end
 end
-
-
