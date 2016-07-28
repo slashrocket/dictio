22c4d35c4a4555304d703c36f22111612d731850
Sean Oldham <seanosaur@me.com>
2016-07-27 19:51:24 -0500

    add Clearance config defaults

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,15 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.allow_sign_up = true # Clearance Default
+  config.cookie_domain = ".dictio.io"
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc } # Clearance Default
+  config.cookie_name = "remember_token" # Clearance Default
+  config.cookie_path = "/" # Clearance Default
+  config.routes = false
+  config.httponly = false # Clearance Default
+  config.mailer_sender = "reply@example.com" # Clearance Default
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt # Clearance Default
+  config.redirect_url = "/" # Clearance Default
+  config.secure_cookie = false # Clearance Default
+  config.sign_in_guards = [] # Clearance Default
+  config.user_model = User # Clearance Default
 end
