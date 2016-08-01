eb44aee010845995883b7c35e84ebc849209aae1
Russell Christensen <russellchristensen@me.com>
2016-08-01 12:59:47 +0000

    Added back serving of static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.assets.compile = false
   
   #Serve static assets
-  #config.serve_static_files = true
+  config.serve_static_files = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
